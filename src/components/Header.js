089c5405b89915c7e188c41ccfb0af7160a2d496
Faizan Alam <faizanalam416@gmail.com>
2024-05-03 00:40:18 +0530

    increasing responsiveness

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,13 +13,13 @@ const Header = ({ theme, setTheme }) => {
     return (
     <div className="bg-[#252525] sticky top-0 border-b-2 z-50 border-black flex justify-between items-center mx-1 mb-1 p-2 font-titleFont dark:bg-[#DDDDDD] dark:border-[#E1F7F5] duration-300">
         <div className="text-yellow-800 p-1 my-1">
-            <h1 className="font-bold text-[#E2482B] text-xl font-serif">Faizan Alam</h1>
+            <h1 className="font-bold text-[#E2482B] xl:text-2xl lg:text-xl md:text-lg sm:text-base text-sm font-serif">Faizan Alam</h1>
         </div>
         <div>
             <ul className="flex text-gray-400 gap-4">
                 {
                     navLinks.map(({id, title, link}) => (
-                        <li className="p-1 m-1 tracking-wide cursor-pointer text-lg hover:text-[#E2482B] duration-300 dark:text-black" key={id}>
+                        <li className="p-1 md:m-1 m-0 tracking-wide cursor-pointer text-lg hover:text-[#E2482B] duration-300 dark:text-black" key={id}>
                             <Link
                                 activeClass="active"
                                 to= {link}
--- src/components/Home.js
+++ src/components/Home.js
@@ -16,8 +16,8 @@ const Home = () => {
     <section id="home" className="flex bg-[#252525] px-2 py-1 m-1 justify-center items-center min-h-60 dark:bg-[#DDDDDD] duration-300">
         <div className="flex justify-around w-5/6">
         <div className="flex flex-col gap-3 w-2/3">
-            <p className="text-white text-2xl dark:text-black">I AM FAIZAN ALAM</p>
-            <h1 className="text-white font-bold text-6xl"> <span className="dark:text-black">a</span> {" "}
+            <p className="text-white xl:text-3xl lg:text-2xl md:text-xl sm:text-lg text-base dark:text-black">I AM FAIZAN ALAM</p>
+            <h1 className="text-white font-bold xl:text-6xl lg:text-5xl md:text-4xl sm:text-3xl text-2xl"> <span className="dark:text-black">a</span> {" "}
                 <span className="text-[#FF4004] ">{text}</span>
                 <Cursor
                     cursorBlinking = "false"
@@ -34,8 +34,8 @@ const Home = () => {
                 </a>
             </div>
         </div>
-        <div className="w-1/3">
-            <img src={myImage} alt="developersImage" className="h-40"/>
+        <div className="w-1/3 flex justify-center items-center">
+            <img src={myImage} alt="developersImage" className="xl:h-44 lg:h-40 md:h-36 sm:h-32 h-28"/>
         </div>
         </div>
     </section>
