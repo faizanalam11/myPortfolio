cbaf3368aa4c8d53dc7af16a416b4db333703e66
Faizan Alam <faizanalam416@gmail.com>
2024-05-16 12:42:18 +0530

    Project Addition

--- src/components/Home.js
+++ src/components/Home.js
@@ -16,7 +16,7 @@ const Home = () => {
     <section id="home" className="flex bg-[#252525] px-2 py-1 m-1 justify-center items-center min-h-60 dark:bg-[#DDDDDD] duration-300">
         <div className="flex justify-around w-5/6">
         <div className="flex flex-col gap-3 w-2/3">
-            <p className="text-white xl:text-3xl lg:text-2xl md:text-xl sm:text-lg text-base dark:text-black">I AM FAIZAN ALAM</p>
+            <p className="text-white font-bold xl:text-3xl lg:text-2xl md:text-xl sm:text-lg text-base dark:text-black">I AM FAIZAN ALAM</p>
             <h1 className="text-white font-bold xl:text-6xl lg:text-5xl md:text-4xl sm:text-3xl text-2xl"> <span className="dark:text-black">a</span> {" "}
                 <span className="text-[#FF4004] ">{text}</span>
                 <Cursor
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,4 +1,5 @@
 import project1 from "../assets/images/project1.png";
+import project2 from "../assets/images/project2.png";
 
 const Projects = () => {
     return (
@@ -7,7 +8,7 @@ const Projects = () => {
                     <p className="flex justify-center items-center font-bold text-[#DC4F25] text-xl font-titleFont">Projects</p>
                 </div>
             <div className='w-5/6 flex flex-wrap justify-evenly items-center mx-auto'>
-                <a href="https://github.com/faizanalam11/aaharVilla.git">
+                <a href="https://github.com/faizanalam11/helloReact">
                     <div className="w-56 bg-[#0a0a0a] text-white my-2 mx-3 items-center justify-evenly rounded-lg hover:scale-105 p-1 duration-300 dark:bg-gray-200 dark:text-black">
                         <div className=' bg-cover'>
                             <img src={project1} alt="developersImage" className='w-full h-[150px]'/>
@@ -21,6 +22,24 @@ const Projects = () => {
                                 <span className='techStack'>NPM</span>
                             </div>
                             <p className='m-1 text-xs'>Creating a food delivery Web app to enshance user's expericence by using Hooks, Lazy Loading and Swiggy's public API</p>
+                        </div>
+                    </div>
+                </a>
+
+                <a href="https://github.com/faizanalam11/newsWebsite">
+                    <div className="w-56 bg-[#0a0a0a] text-white my-2 mx-3 items-center justify-evenly rounded-lg hover:scale-105 p-1 duration-300 dark:bg-gray-200 dark:text-black">
+                        <div className=' bg-cover'>
+                            <img src={project2} alt="developersImage" className='w-full h-[150px]'/>
+                        </div>
+                        <div>
+                            <h3 className="font-bold m-1 text-xl">News Maniac</h3>
+                            <div className='flex flex-row flex-wrap gap-2 m-1'>
+                                <span className='techStack'>React.Js</span>
+                                <span className='techStack'>HTML</span>
+                                <span className='techStack'>Tailwind</span>
+                                <span className='techStack'>NPM</span>
+                            </div>
+                            <p className='m-1 text-xs'>News Maniac, a dynamic news platform that brings you the latest headlines, stories, and updates from around the globe. 🌎📰</p>
                         </div>
                     </div>
                 </a>
