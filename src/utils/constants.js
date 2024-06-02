21b2c948715436e99c9096e95aaef709b93ff1d9
Faizan Alam <faizanalam416@gmail.com>
2024-06-02 12:19:45 +0530

    Resume--update

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 export const linkedIn = "https://www.linkedin.com/in/faizanalam123/";
 
-export const Resume = "https://drive.google.com/file/d/1vwTlsNEXYsdJBbWqmW9eUWKe2lFwT6LZ/view?usp=sharing";
+export const Resume = "https://drive.google.com/file/d/1g3F7FI5blo-y6DuBjruX3kvAWRAl3z5z/view?usp=drive_link";
 
 export const navLinks = [
     {
