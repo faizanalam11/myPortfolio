3a97b62f177ee56480b5b11e7d7eda7be61ddec2
Faizan Alam <faizanalam416@gmail.com>
2024-09-05 02:20:31 +0530

    Working on Home sections

--- src/components/Home.js
+++ src/components/Home.js
@@ -15,8 +15,8 @@ const Home = () => {
       })
     return (
     <section id="home" className="flex text-[#ECEEEF] bg-[rgb(31,31,31)] px-2 py-1 m-1 justify-center items-center min-h-60 dark:bg-[#DDDDDD] duration-300 dark:text-[rgb(31,31,31)]">
-        <div className="flex justify-around w-5/6">
-            <div className="flex flex-col gap-3 w-2/3">
+        <div className="flex flex-col sm:flex-row justify-around w-5/6">
+            <div className="flex flex-col gap-3 w-2/3 order-2 sm:order-2 justify-center">
                 <p className="font-bold xl:text-3xl lg:text-2xl md:text-xl sm:text-lg text-base">I AM FAIZAN ALAM</p>
                 <h1 className="font-bold xl:text-6xl lg:text-5xl md:text-4xl sm:text-3xl text-2xl"> <span className="">a</span> {" "}
                     <span className="text-[#91E0EE] dark:text-[#087E96]">{text}</span>
@@ -26,7 +26,7 @@ const Home = () => {
                         cursorColor="#91E0EE"
                     />
                 </h1>
-                <div className="flex flex-row gap-4 mt-2 items-center">
+                <div className="flex flex-col sm:flex-row gap-4 mt-2 items-center">
                     <a href={linkedIn} target="_blank">
                         <button className="border border-1 border-zinc-50 dark:text-[#ECEEEF] dark:bg-[#087E96] text-white p-[0.35rem] rounded-3xl font-semibold hover:bg-opacity-40 capitalize hover:bg-slate-500">Contact Me</button>
                     </a>
@@ -35,7 +35,7 @@ const Home = () => {
                     </a>
                 </div>
             </div>
-            <div className="w-1/3 flex justify-center items-center">
+            <div className="w-1/3 flex justify-center items-center order-1 sm:order-2">
                 <img src={myImage} alt="developersImage" className="xl:h-44 lg:h-40 md:h-36 sm:h-32 h-28 rounded-full"/>
             </div>
         </div>
