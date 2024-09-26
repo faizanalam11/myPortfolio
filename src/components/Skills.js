1c523c6d3f7adeb7753c4c537c620b22f3418621
Faizan Alam <faizanalam416@gmail.com>
2024-09-26 18:16:21 +0530

    Some Improvement's in skills section

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,5 +1,5 @@
 import { FaReact, FaNodeJs, FaHtml5 } from "react-icons/fa";
-import { SiJavascript, SiTailwindcss } from "react-icons/si";
+import { SiJavascript, SiTailwindcss, SiExpress } from "react-icons/si";
 import { DiMsqlServer } from "react-icons/di";
 import { TbBrandCpp } from "react-icons/tb";
 
@@ -10,32 +10,35 @@ const Skills = () => {
                 <p className="font-bold text-[#91E0EE] dark:text-[#087E96] text-xl font-titleFont">Skills</p>
             </div>
             <div className="w-5/6 m-auto flex">
-            <div className="flex flex-wrap justify-evenly">
-                <span className="skillsIcon text-2xl"> 
-                    <TbBrandCpp/>
-                </span>
-                <span className="skillsIcon"> 
-                    <FaHtml5/>
-                </span>
-                <span className="skillsIcon">
-                    <SiTailwindcss/>
-                </span>
-                <span className="skillsIcon"> 
-                    <SiJavascript/>
-                </span>
-                <span className="skillsIcon"> 
-                    <FaReact/>
-                </span>
-                <span className="skillsIcon"> 
-                    <FaNodeJs/>
-                </span>
-                <span className="skillsIcon text-4xl">
-                    <DiMsqlServer/>
-                </span>
-                <span className="skillsIcon">
-                    <p>DSA</p>
-                </span>
-            </div>
+                <div className="flex flex-wrap m-auto justify-evenly">
+                    <span className="skillsIcon text-2xl"> 
+                        <TbBrandCpp/>
+                    </span>
+                    <span className="skillsIcon"> 
+                        <FaHtml5/>
+                    </span>
+                    <span className="skillsIcon">
+                        <SiTailwindcss/>
+                    </span>
+                    <span className="skillsIcon"> 
+                        <SiJavascript/>
+                    </span>
+                    <span className="skillsIcon"> 
+                        <FaReact/>
+                    </span>
+                    <span className="skillsIcon"> 
+                        <FaNodeJs/>
+                    </span>
+                    <span className="skillsIcon"> 
+                        <SiExpress/>
+                    </span>
+                    <span className="skillsIcon text-4xl">
+                        <DiMsqlServer/>
+                    </span>
+                    <span className="skillsIcon">
+                        <p>DSA</p>
+                    </span>
+                </div>
             </div>
         </section>
     );
